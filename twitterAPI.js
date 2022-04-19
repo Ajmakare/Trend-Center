3cb707098b54cb41dfb4f0a37d28f20b95fdbf88
Aidan Makare <aidanmakaree@gmail.com>
2022-04-18 19:47:06 -0500

    Changed API call logic

--- twitterAPI.js
+++ twitterAPI.js
@@ -1,13 +1,25 @@
-window.onload = () =>{
-    $.ajax({
-        url: 'https://api.twitter.com/1.1/trends/place.json?id=1',
-        type: 'GET',
-        contentType: 'application/json',
-        headers:{
-            'Authorization': '',
-        },
-        success: function(result){
-            //store to API, not UI
-        }
-    })
+async function getTwitterJSON() {
+  console.log("Getting Twitter JSON");
+  const result = fetch("https://api.twitter.com/1.1/trends/place.json?id=1", {
+    type: "GET",
+    contentType: "application/json",
+    headers: {
+      Authorization: "",
+    },
+  });
+  return result;
 }
+
+// window.onload = () => {
+//   $.ajax({
+//     url: "https://api.twitter.com/1.1/trends/place.json?id=1",
+//     type: "GET",
+//     contentType: "application/json",
+//     headers: {
+//       Authorization: "",
+//     },
+//     success: function (result) {
+//       //store to API, not UI
+//     },
+//   });
+// };
