d4ef08fdbe380bf1a5c013f4845ecc4807bc7f60
Mark Williams <markwilliams@marks-mbp-2.lan>
2022-04-20 20:46:33 -0500

    twitter pulls back

--- node-api-postgres/twitterAPI.js
+++ node-api-postgres/twitterAPI.js
@@ -7,10 +7,10 @@ export default async function getTwitterJSON() {
       method: "GET",
       headers: {
         Authorization:
-          "Bearer AAAAAAAAAAAAAAAAAAAAAOTRawEAAAAA3yjRT%2F5OMTMYuIwhgRWvkAHPinE%3DNzdt5w482e6axWPZdCeyQzEJHdoPSLD5HEWdjcfy9PPtxyXc6N",
-        AqRmngtb6C8oKcsdCSNiC8chd: "AqRmngtb6C8oKcsdCSNiC8chd",
+          
+        AqRmngtb6C8oKcsdCSNiC8chd: ,
         Cookie:
-          'guest_id=v1%3A165032082139279871; guest_id_ads=v1%3A165032082139279871; guest_id_marketing=v1%3A165032082139279871; personalization_id="v1_ZCxSjhUrWmdXLIaQOCdsng=="',
+          
       },
     }
   )
