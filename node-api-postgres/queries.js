076e2376b735a63dc907e527f62fd8c3f86fc77d
Mark Williams <markwilliams@marks-mbp-2.lan>
2022-04-20 20:45:54 -0500

    twitter API Pulls data

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -1,77 +1,83 @@
-const Pool = require("pg").Pool; //Environment
-const getTwitter = require("./twitterAPI.js");
+// const Pool = require("pg").Pool //Environment
+import Pool from "pg"
+// const getTwitter = require("./twitterAPI.js")
+import getTwitterJSON from "./twitterAPI.js"
 
-const pool = new Pool({
+const pgPool = Pool.Pool
+
+const pool = new pgPool({
   user: "me",
   host: "localhost",
   database: "api",
   password: "password",
   port: 5432,
-});
+})
 
 const getUsers = (request, response) => {
-  console.log("WORKS");
+  console.log("WORKS")
   pool.query("SELECT * FROM twitterdata", (error, results) => {
     if (error) {
-      throw error;
+      throw error
     }
-    response.status(200).json(results.rows);
-  });
-};
+    response.status(200).json(results.rows)
+  })
+}
 
 const getUser = (request, response) => {
-  const id = parseInt(request.params.id);
+  const id = parseInt(request.params.id)
   pool.query(
     "SELECT * FROM user_info WHERE id= $1 ", //$1 is our selector
     [id],
     (error, results) => {
       if (error) {
-        throw error;
+        throw error
       }
-      response.status(200).json(results.rows);
+      response.status(200).json(results.rows)
     }
-  );
-};
-const getTweets = (request, response) => {
-  console.log("hits");
-  getTwitter.getTwitterJSON();
-  console.log(twitterData);
-  response.status(200);
-};
+  )
+}
+
+const getTweets = async (request, response) => {
+  console.log("hits")
+  const result = await getTwitterJSON()
+  console.log(result)
+
+  return response.status(200).json(result)
+}
 
 const updateUser = (request, response) => {
-  console.log("hits");
-  const id = parseInt(request.params.id);
-  const { name, age } = request.body;
+  console.log("hits")
+  const id = parseInt(request.params.id)
+  const { name, age } = request.body
 
   pool.query(
     "UPDATE user_info SET name = $1, age = $2 WHERE id = $3",
     [name, age, id],
     (error, results) => {
       if (error) {
-        throw error;
+        throw error
       }
-      response.status(200).send(`User modified with ID: ${id}`);
+      response.status(200).send(`User modified with ID: ${id}`)
     }
-  );
-};
+  )
+}
 
 const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id);
-  console.log(id);
+  const id = parseInt(request.params.id)
+  console.log(id)
 
   pool.query("DELETE FROM user_info WHERE id= $1", [id], (error, results) => {
     if (error) {
-      throw error;
+      throw error
     }
-    response.status(200).send(`User Deleted`);
-  });
-};
+    response.status(200).send(`User Deleted`)
+  })
+}
 
-module.exports = {
+export default {
   getUsers,
   getUser,
   getTweets,
   updateUser,
   deleteUser,
-};
+}
--- node-api-postgres/twitterAPI.js
+++ node-api-postgres/twitterAPI.js
@@ -1,13 +1,19 @@
-async function getTwitterJSON() {
-  fetch("https://api.twitter.com/1.1/trends/place.json?id=1", {
-    method: "GET",
-    headers: {},
-  })
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-}
+import fetch from "node-fetch"
 
-module.exports = {
-  getTwitterJSON,
-};
+export default async function getTwitterJSON() {
+  const result = await fetch(
+    "https://api.twitter.com/1.1/trends/place.json?id=1",
+    {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Bearer AAAAAAAAAAAAAAAAAAAAAOTRawEAAAAA3yjRT%2F5OMTMYuIwhgRWvkAHPinE%3DNzdt5w482e6axWPZdCeyQzEJHdoPSLD5HEWdjcfy9PPtxyXc6N",
+        AqRmngtb6C8oKcsdCSNiC8chd: "AqRmngtb6C8oKcsdCSNiC8chd",
+        Cookie:
+          'guest_id=v1%3A165032082139279871; guest_id_ads=v1%3A165032082139279871; guest_id_marketing=v1%3A165032082139279871; personalization_id="v1_ZCxSjhUrWmdXLIaQOCdsng=="',
+      },
+    }
+  )
+  const data = await result.json()
+  return data
+}
